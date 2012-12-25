008817e078d23c287f5f8c13891c7ba1b8aca46d
Wyatt Johnson <wyattjoh@gmail.com>
2012-12-24 19:52:45 -0700

    Ini commit

---
+++ server_backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ACCOUNT="$1"
+DATE=`date +%s`
+FILENAME="$ACCOUNT_$DATE.tar.bz2"
+
+tar cjf /root/"$FILENAME" /backup/cpbackup/weekly/"$ACCOUNT"/
+bash dropbox_uploader.sh upload /root/"$FILENAME"
+rm -rf /root/"$FILENAME"
