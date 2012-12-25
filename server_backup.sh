7dab068cdb23d5afade55b8d999b0c6d5d3112d2
Wyatt Johnson <wyattjoh@gmail.com>
2012-12-24 19:58:55 -0700

    Fixed stupid var error

--- server_backup.sh
+++ server_backup.sh
@@ -2,7 +2,7 @@
 
 ACCOUNT="$1"
 DATE=`date +%s`
-FILENAME="$ACCOUNT_$DATE.tar.bz2"
+FILENAME="${ACCOUNT}_${DATE}.tar.bz2"
 
 tar cjf /root/"$FILENAME" /backup/cpbackup/weekly/"$ACCOUNT"/
 bash dropbox_uploader.sh upload /root/"$FILENAME"
